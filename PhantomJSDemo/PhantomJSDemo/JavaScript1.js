bfe1a0aa1a7eda933ac4a708c47dd5be0688ab6a
zuorenshuai@taolx.com <zuorenshuai@taolx.com>
2016-07-11 11:34:30 +0800

    初建

---
+++ PhantomJSDemo/PhantomJSDemo/JavaScript1.js
@@ -0,0 +1,42 @@
+﻿var page = require('webpage').create();
+var system = require("system");
+var pageParams = { rootDir: "", env: "test", jquery: "" };
+var url = 'https://accounts.taolx.com';
+pageParams = JSON.parse(system.stdin.readLine());
+
+/*返回方法*/
+var doResult = function (result) {
+    if (typeof result !== "string")
+        result = JSON.stringify(result)
+    console.log("<`R>" + result + "</~R>");
+}
+
+/*数据抓取*/
+var grab = function (args) {
+    var result = { s: "", b: "" };
+    return document.cookie;
+}
+
+/*测试环境下输出页面控制台log*/
+if (pageParams.env == "test") {
+    page.onConsoleMessage = function (msg) {
+        console.log('PCL:', msg);
+    };
+}
+
+/*加载页面*/
+page.open(url, function (status) {
+    if (status !== "success") {
+        console.log('FAIL to load the address', url);
+        phantom.exit();
+        return;
+    }
+    /*加载jQuery*/
+    page.includeJs(pageParams.jquery, function () {
+        var result = page.evaluate(grab);
+        doResult(result);
+        phantom.exit()
+    });
+});
+
+
---
+++ PhantomJSDemo/PhantomJSDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using NReco.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PhantomJSDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Action act = () =>
+            {
+                var phantomJS = new PhantomJS();
+                phantomJS.OutputReceived += (sender, e) =>
+                {
+                    Console.WriteLine("PhantomJS output: {0}", e.Data);
+                };
+                var js = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "JavaScript1.js");
+                var pageParams = new
+                {
+                    rootDir = AppDomain.CurrentDomain.BaseDirectory,
+                    env = "pro",
+                    jquery = "http://static.taolx.com/common/js/jquery-1.9.0.min.js"                     
+                };
+                var inputStr = JsonConvert.SerializeObject(pageParams);
+                var inputStream = new MemoryStream(Encoding.UTF8.GetBytes(inputStr + "\n"));
+                using (var ops = new MemoryStream())
+                {
+                    phantomJS.RunScript(js, null, inputStream, ops);
+                    var str = Encoding.UTF8.GetString(ops.ToArray());
+                    Console.WriteLine(str);
+                    Console.WriteLine(GetValue(str, "<`R>", "</~R>"));
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            };
+            //act.BeginInvoke(null, null);
+            //act.BeginInvoke(null, null);
+            //act.BeginInvoke(null, null);
+            for (var index = 0; index < 2; index++)
+                act.BeginInvoke(null, null);
+            Console.ReadLine();
+        }
+
+
+
+        /// <summary>
+        /// 获得字符串中开始和结束字符串中间得值
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="s">开始</param>
+        /// <param name="e">结束</param>
+        /// <returns></returns> 
+        public static string GetValue(string str, string s, string e)
+        {
+            Regex rg = new Regex("(?<=(" + s + "))[.\\s\\S]*?(?=(" + e + "))", RegexOptions.Multiline | RegexOptions.Singleline);
+            return rg.Match(str).Value;
+        }
+    }
+}
